5c999951fef9f69e123a44f505aeb7800951f227
EllieWalsch <elliewalsch4@gmail.com>
2022-10-25 21:23:14 -0500

    Fix classwork

--- src/index.js
+++ src/index.js
@@ -7,5 +7,25 @@ const h2 = document.createElement("h2");
 const small = document.createElement("small");
 const p = document.createElement("p");
 
-// submit event listener
-form.addEventListener("submit", function () {});
+// submit event listener, but default the browser will refresh
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // prevent.Default will prevent the refresh
+
+  // event.target - think of callerID - what caused the event
+  const fD = new FormData(event.target);
+
+  // takes the input and pairs it with the values (what users type in)
+  const values = Object.fromEntries(fD.entries());
+
+  // Object Destructuring - grabs values for each
+  const { title, subtitle, author, content } = values;
+
+  h1.textContent = title;
+  h2.textContent = subtitle;
+  small.textContent = author;
+  p.textContent = content;
+
+  // puts textContent into main, publishes content into the form
+  main.append(h1, h2, small, p);
+});
