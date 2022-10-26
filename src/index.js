92d78d24c3ff5916f3d6aaffdb6c1bb0eb1267ed
EllieWalsch <elliewalsch4@gmail.com>
2022-10-25 20:49:06 -0500

    Add classwork

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 const form = document.querySelector("form");
 const main = document.querySelector("main");
+
+// create new elements for each tag in html
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h2");
+const small = document.createElement("small");
+const p = document.createElement("p");
+
+// submit event listener
+form.addEventListener("submit", function () {});
